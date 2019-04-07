1d5a86a89b1ce8bf510ca6d8ce4f158a2ed38569
hjorthjort <rikard.hjort@gmail.com>
2019-04-07 15:29:33 +0200

    (Naively) remove duplicates

--- src/Listr.hs
+++ src/Listr.hs
@@ -12,24 +12,28 @@ import System.Directory (
   , getSymbolicLinkTarget
   )
 import System.FilePath.Posix ((</>), makeRelative, takeDirectory)
+import Data.List (nub)
 
 -- | Recursively get all non-hidden (starting with ".") files from a given directory.
 -- Follows symbolic links.
 listContents :: FilePath -> IO [String]
-listContents path = do contents <- getDirectoryContents path
-                       currPath <- getCurrentDirectory
-                       -- A list of conditions the contents of the path must satisfy.
-                       let conds = [
-                             (/= '.').head -- Ignore hidden (Linux only).
-                             ] :: [FilePath -> Bool]
-                           filtered = filter (\x -> all ($x) conds) contents
-                           paths = map ((path </>)) filtered
-                       files <- onlyFiles paths
-                       dirs  <- onlyDirs paths
-                       recursiveFiles <- concat <$> mapM listContents dirs
-                       let uncleanRes = files ++ recursiveFiles
-                           cleanedRes = makeRelative currPath <$> uncleanRes
-                       return cleanedRes
+listContents path = nub <$> listContents' path
+
+listContents' :: FilePath -> IO [String]
+listContents' path = do contents <- getDirectoryContents path
+                        currPath <- getCurrentDirectory
+                        -- A list of conditions the contents of the path must satisfy.
+                        let conds = [
+                              (/= '.').head -- Ignore hidden (Linux only).
+                              ] :: [FilePath -> Bool]
+                            filtered = filter (\x -> all ($x) conds) contents
+                            paths = map ((path </>)) filtered
+                        files <- onlyFiles paths
+                        dirs  <- onlyDirs paths
+                        recursiveFiles <- concat <$> mapM listContents dirs
+                        let uncleanRes = files ++ recursiveFiles
+                            cleanedRes = makeRelative currPath <$> uncleanRes
+                        return cleanedRes
   where
     onlyFiles :: [FilePath] -> IO [FilePath]
     onlyFiles = filterPaths doesFileExist
