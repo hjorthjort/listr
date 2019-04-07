9f26a65b90503401623a4f574e6207790ca6795e
hjorthjort <rikard.hjort@gmail.com>
2019-04-07 16:04:59 +0200

    Robustness: allow file paths and bad paths as input

--- src/Listr.hs
+++ src/Listr.hs
@@ -2,6 +2,7 @@ module Listr
     ( listContents
     ) where
 
+import System.IO (stderr, hPutStrLn)
 import System.Directory (
     getDirectoryContents
   , doesFileExist
@@ -17,9 +18,16 @@ import System.FilePath.Posix ((</>), makeRelative, takeDirectory)
 -- | Recursively get all non-hidden (starting with ".") files from a given directory.
 -- Follows symbolic links.
 listContents :: FilePath -> IO [String]
-listContents path = nub <$> listContents' path
+listContents path = do path' <- filterPaths doesPathExist [path]
+                       case path' of
+                         [] -> warn path >> return []
+                         [p] -> do isFile <- doesFileExist p
+                                   if isFile
+                                     then return ["." </> p]
+                                     else listContents' p
+  where warn path = hPutStrLn stderr $ "Warning: " ++ path ++ " is not a valid path"
 
-listContents' :: FilePath -> IO [String]
+listContents' :: FilePath -> IO [FilePath]
 listContents' path = do contents <- getDirectoryContents path
                         currPath <- getCurrentDirectory
                         -- A list of conditions the contents of the path must satisfy.
@@ -41,18 +49,23 @@ listContents' path = do contents <- getDirectoryContents path
     onlyDirs = filterPaths doesDirectoryExist
 
 -- | Retain only paths matching the given (monadic) predicate.
--- Follows symbolic links.
+-- Follows symbolic links and replace paths with what they point to.
+-- Drop paths that do not point anywhere.
 filterPaths :: (FilePath -> IO Bool) -> [FilePath] -> IO [FilePath]
 filterPaths _     []     = return []
-filterPaths predM (f:fs) = do sym   <- pathIsSymbolicLink f
-                              rest  <- filterPaths predM fs
-                              if sym
-                                then do targ <- getSymbolicLinkTarget f
-                                        let dir = takeDirectory f
-                                            f'  = dir </> targ
-                                        exist <- doesPathExist f'
-                                        if exist
-                                          then filterPaths predM (f':fs)
-                                          else return rest
-                                else do valid <- predM f
-                                        return $ if valid then f:rest else rest
+filterPaths predM (f:fs) =
+  do exist <- doesPathExist f
+     rest <- filterPaths predM fs
+     if not exist
+       then return rest
+       else do sym <- pathIsSymbolicLink f
+               if sym
+                 then do targ <- getSymbolicLinkTarget f
+                         let dir = takeDirectory f
+                             f'  = dir </> targ
+                         exist <- doesPathExist f'
+                         if exist
+                           then filterPaths predM (f':fs)
+                           else return rest
+                 else do valid <- predM f
+                         return $ if valid then f:rest else rest
